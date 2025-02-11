812392796e119456c33934ebb7003e78ed1f5573
Titiss77 <Mathis.fcs.lav@gmail.com>
2025-02-11 14:24:58 +0100

    Update extention.js

--- extention.js
+++ extention.js
@@ -1,10 +1,10 @@
 // extensions.js
 function extentionNightflix() {
     // Exemple de correspondance d'ID avec des extensions (cela peut venir d'une source externe)
-    const extensions = ".pics";
+    const extensions = ".fesses";
 
     // Retourne l'extension correspondante selon l'ID
-    return extensions[id] || ".pics"; // Par défaut, retourne .pics si l'ID n'est pas trouvé
+    return extensions || ".pics"; // Par défaut, retourne .pics si l'ID n'est pas trouvé
 }
 
 // Chargement du fichier JSON
